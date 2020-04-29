63afb9ad40ed379a71fea10f947ebf30b8547a02
sahal alzubair <inzilagiway@gmail.com>
2020-04-29 22:55:50 +0300

    added manual/simv mode, simv mode lacks calculations.

--- arduino_code/mega_code/mega_code.ino
+++ arduino_code/mega_code/mega_code.ino
@@ -18,6 +18,7 @@ const int speedIncBtn = 21; // speed value is controlled by changing the delay i
 const int speedDecBtn = 20;
 const int volIncBtn = 19; // volume value is controlled by changing the target angle
 const int volDecBtn = 18;
+const int modeChangeBtn = 2; // this button changes the ventilation working mode
 
 //objects
 Servo servo1; // create servo object
@@ -26,6 +27,7 @@ SFM3000wedo measflow(64); //sensor flow object, this device uses I2C communicati
 //misc
 int offset = 32000; // Offset for the flow sensor #TODO: revision this
 float scale = 140.0; // Scale factor for Air and N2 is 140.0, O2 is 142.8 #TODO: revision this
+int ventilationMode = 0; // 0 = manual mode
 
 /* speed variables */
 int currentSpeed = 16; // 16 times per minute, half-cycle every 1.875s
@@ -79,6 +81,10 @@ bool safetyCheck() { // checks for and corrects conditions that could cause harm
 }
 
 void increaseSpeed() {
+    if(ventilationMode != 0) {
+      Serial.println("please change to manual mode to change speed manually.");
+      return;
+    }
     delay(50);
     currentSpeed += speedInc;
     Serial.print("Speed increased, current speed value is ");
@@ -86,6 +92,10 @@ void increaseSpeed() {
 }
 
 void decreaseSpeed() {
+    if(ventilationMode != 0) {
+      Serial.println("please change to manual mode to change speed manually.");
+      return;
+    }
     delay(50);
     currentSpeed -= speedDec;
     Serial.print("Speed decreased, current speed value is ");
@@ -93,6 +103,10 @@ void decreaseSpeed() {
 }
 
 void increaseVolume() {
+    if(ventilationMode != 0) {
+      Serial.println("please change to manual mode to change angle manually.");
+      return;
+    }
     delay(50);
     targetAngle += angleInc;
     Serial.print("Target angle increased, current angle value is ");
@@ -100,53 +114,62 @@ void increaseVolume() {
 }
 
 void decreaseVolume() {
+    if(ventilationMode != 0) {
+      Serial.println("please change to manual mode to change angle manually.");
+      return;
+    }
     delay(50);
     targetAngle -= angleDec;
     Serial.print("Target angle decreased, current angle value is ");
     Serial.println(targetAngle);
 }
 
-float readSensors(){
+float getFlow(){
     // reading flow
     unsigned int flowSensorResult = measflow.getvalue(); // get the reading from the flow sensor
     float flow = ((float)result - offset) / scale; // some math magic to get the actual flow value #TODO: test and check this value (expected: slm)
     // a calculation to get the tidal volume value from the flow goes here. #TODO revisit this 
-    Serial.print("The flow value is ");
-    Serial.print(flow);
-    Serial.print(" slm");
+    return flow;
+}
+
+float getPresure() {
     // reading pressure value
     unsigned int = prsesureSensorResult = analogRead(pressureSensorPin);
     // a calculation to get the Pa value from the analog voltage value goes here #TODO
     /*Serial.print();
     Serial.print();
-    Serial.print();*/
+    Serial.print();
+    return pressureValue; */
 }
 
-void setup() {
-  pinMode(speedIncBtn, INPUT);
-  pinMode(speedDecBtn, INPUT);
-  pinMode(volIncBtn, INPUT);
-  pinMode(volDecBtn, INPUT);
-  attachInterrupt(digitalPinToInterrupt(speedIncBtn), increaseSpeed, RISING);
-  attachInterrupt(digitalPinToInterrupt(speedDecBtn), decreaseSpeed, RISING);
-  attachInterrupt(digitalPinToInterrupt(volIncBtn), increaseVolume, RISING);
-  attachInterrupt(digitalPinToInterrupt(volDecBtn), decreaseVolume, RISING);
-  servo1.attach(servoPin);
-  servo1.write(minAngle);
-  Wire.begin();
-  Serial.begin(9600);
-  delay(500); // delay to set the servo and startup the serial console
-  measflow.init();
-  Serial.println("Flow sensor initilized.");
+void changeMode() { // this function changes the workimg mode of the ventilator
+  if(ventilationMode == 0) {
+    ventilationMode++; 
+    Serial.println("Ventilation mode: manual");
+  } else {
+    Serial.println("Ventilation mode: SIMV");
+    ventilationMode = 0;
+  }
 }
 
-void loop() {
-  if(safetyCheck()) { // if the device passes the safety check, start working immediately
-    readSensors();
+void displayValues() {
     Serial.print("target angle : ");
     Serial.println(targetAngle);
     Serial.print("current speed : ");
     Serial.println(currentSpeed);
+    float flow = getFlow();
+    Serial.print("The flow value is ");
+    Serial.print(flow);
+    Serial.print(" slm");
+    /* float pressure = getPresure();
+     *  Serial.print("The pressure value is ");
+     *  Serial.print(pressure);
+     *  Serial.print(" Pa");
+    */
+}
+
+void manualVentilation() {
+    displayValues();
     for(int i = minAngle; i <= targetAngle; i++) { // move the servo from 0 to the target angle
       servo1.write(i);
       delay(getDelay(currentSpeed, true)); // get the needed delay to keep the process at the desired current speed
@@ -156,5 +179,66 @@ void loop() {
       servo1.write(i);
       delay(getDelay(currentSpeed, false)); // get the needed delay for the return trip speed
     }
+}
+
+void smivVentilation() {
+  displayValues();
+  float flow = getFlow();
+  // turn flow to tidial volume here
+  // a calculation to determine the needed angle and speed goes here
+  /* 
+   *  int startingAnglesimv = ...;
+   *  int targetAnglesimv = ...;
+   *  int speedsimv = ...;
+   *  for(int i = startingAnglesimv; i <= targetAnglesimv; i++) { // move the servo from 0 to the target angle
+      servo1.write(i);
+      delay(getDelay(speedsimv, true)); // get the needed delay to keep the process at the desired current speed
+    }
+    delay(pauseDelay); // delay between inhaling and exhaling
+    for(int i = targetAnglesimv; i >= startingAnglesimv; i--) { // move the servo from the current target angle to 0
+      servo1.write(i);
+      delay(getDelay(speedsimv, false)); // get the needed delay for the return trip speed
+    }
+   */
+}
+
+void setup() {
+  pinMode(speedIncBtn, INPUT);
+  pinMode(speedDecBtn, INPUT);
+  pinMode(volIncBtn, INPUT);
+  pinMode(volDecBtn, INPUT);
+  pinMode(modeChangePin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(speedIncBtn), increaseSpeed, RISING);
+  attachInterrupt(digitalPinToInterrupt(speedDecBtn), decreaseSpeed, RISING);
+  attachInterrupt(digitalPinToInterrupt(volIncBtn), increaseVolume, RISING);
+  attachInterrupt(digitalPinToInterrupt(volDecBtn), decreaseVolume, RISING);
+  attachInterrupt(digitalPinToInterrupt(modeChangeBtn), changeMode, RISING);
+  servo1.attach(servoPin);
+  servo1.write(minAngle);
+  Wire.begin();
+  Serial.begin(9600);
+  delay(500); // delay to set the servo and startup the serial console
+  measflow.init();
+  Serial.println("Flow sensor initilized.");
+  Serial.println("Ventilation mode: manual");
+  Serial.print("Current speed : ");
+  Serial.println(currentSpeed);
+  Serial.print("Current angle : ");
+  Serial.println(targetAngle);
+  delay(500);
+}
+
+void loop() {
+  if(safetyCheck()) { // if the device passes the safety check, start working immediately
+    switch(ventilationMode) {
+      case 0: // manual mode
+        manualVentilation();
+        break;
+       case 1: // SIMV mode
+        simvVentilation();
+        break;    
+    }    
+  } else {
+    Serial.println("Safety error, please contact technical support and disconnect this device immdeiately."); // #TODO add error codes for more info
   }
 }
